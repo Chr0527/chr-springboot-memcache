d76d9bbbb3a6eb22ffb57f5ecacd9029fe5d2d23
Chr0527 <1015670600@qq.com>
2019-07-02 18:20:43 +0800

    Initial commit

---
+++ src/main/java/com/chryl/ChrSpringbootMemcacheApplication.java
@@ -0,0 +1,13 @@
+package com.chryl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChrSpringbootMemcacheApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChrSpringbootMemcacheApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/chryl/controller/MemcacheController.java
@@ -0,0 +1,47 @@
+package com.chryl.controller;
+
+import com.whalin.MemCached.MemCachedClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Date;
+
+/**
+ * 总结：memcache通过set方法把值存入都memcache缓存中；通过get方法把值取出来；通过设置过期时间，使其失效。
+ * <p>
+ * Created By Chr on 2019/7/2.
+ */
+@Controller
+public class MemcacheController {
+
+    @Autowired
+    private MemCachedClient memCachedClient;
+
+    /**
+     * memcache缓存
+     */
+    @RequestMapping("/memcache")
+    @ResponseBody
+    public String memcache() throws InterruptedException {
+        // 放入缓存
+        boolean flag = memCachedClient.set("mem", "name");
+        // 取出缓存
+        Object value = memCachedClient.get("mem");
+        System.out.println(value);
+        // 3s后过期
+        memCachedClient.set("num", "666", new Date(3000));
+        /*memCachedClient.set("num", "666", new Date(System.currentTimeMillis()+3000));与上面的区别是当设置了这个时间点
+        之后，它会以服务端的时间为准，也就是说如果本地客户端的时间跟服务端的时间有差值，这个值就会出现问题。
+        例：如果本地时间是20:00:00,服务端时间为20:00:10，那么实际上会是40秒之后这个缓存key失效*/
+        Object key = memCachedClient.get("num");
+        System.out.println(key);
+        //多线程睡眠3s
+        Thread.sleep(3000);
+        key = memCachedClient.get("num");
+        System.out.println(value);
+        System.out.println(key);
+        return "success";
+    }
+}
